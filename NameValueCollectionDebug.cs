17ff7995859b756336e4fccc0b3b4573db5f3042
radek.jancik@sunamo.cz <radek.jancik@sunamo.cz>
2023-11-30 21:42:27 +0100

    init

---
+++ NameValueCollectionDebug.cs
@@ -0,0 +1,15 @@
+public class NameValueCollectionDebug
+{
+    public static void Print(NameValueCollection nvc)
+    {
+        foreach (var item in nvc.AllKeys)
+        {
+            d(item + ": " + nvc[item]);
+        }
+    }
+
+    private static void d(string v)
+    {
+        Debug.WriteLine(v);
+    }
+}
---
+++ SqlServerHelperDebug.cs
@@ -0,0 +1,16 @@
+public class SqlServerHelperDebug
+{
+    static Type type = typeof(SqlServerHelperDebug);
+
+    /// <summary>
+    /// Musím zde předat přímo Dict a nezjištovat si počty řádků až zde - to bych musel referencovat SunamoSqlServer a nemohl bych tak používat SunamoDebugging in SunamoSqlServer
+    /// 
+    /// The same TextOutputGenerator in sunamo - 
+    /// </summary>
+    /// <param name="l"></param>
+    /// <returns></returns>
+    public static void RowsInTables(Dictionary<string, int> l)
+    {
+        ThrowEx.NotImplementedMethod();
+    }
+}
