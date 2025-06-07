2d48dc7f645dc86537b812321621d405c7433f0a
sunamo.cz <radek.jancik@sunamo.cz>
2025-06-07 20:20:07 +0200

    25.6.7.1 Removed unused method parameters

--- SqlServerHelperDebug.cs
+++ SqlServerHelperDebug.cs
@@ -1,18 +1,18 @@
-namespace SunamoDebugging;
+//namespace SunamoDebugging;
 
-public class SqlServerHelperDebug
-{
-    private static Type type = typeof(SqlServerHelperDebug);
+//public class SqlServerHelperDebug
+//{
+//    private static Type type = typeof(SqlServerHelperDebug);
 
-    /// <summary>
-    ///     Musím zde předat přímo Dict a nezjištovat si počty řádků až zde - to bych musel referencovat SunamoSqlServer a
-    ///     nemohl bych tak používat SunamoDebugging in SunamoSqlServer
-    ///     The same TextOutputGenerator in sunamo -
-    /// </summary>
-    /// <param name="l"></param>
-    /// <returns></returns>
-    public static void RowsInTables(Dictionary<string, int> l)
-    {
-        ThrowEx.NotImplementedMethod();
-    }
-}
+//    /// <summary>
+//    ///     Musím zde předat přímo Dict a nezjištovat si počty řádků až zde - to bych musel referencovat SunamoSqlServer a
+//    ///     nemohl bych tak používat SunamoDebugging in SunamoSqlServer
+//    ///     The same TextOutputGenerator in sunamo -
+//    /// </summary>
+//    /// <param name="l"></param>
+//    /// <returns></returns>
+//    public static void RowsInTables(Dictionary<string, int> l)
+//    {
+//        ThrowEx.NotImplementedMethod();
+//    }
+//}
